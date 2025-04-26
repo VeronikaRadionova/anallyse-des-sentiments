90fcfc42cc2c170e06408b7e056d5404f1da02c4
VeronikaRadionova <114427643+VeronikaRadionova@users.noreply.github.com>
2025-04-26 23:35:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ fig_pie = go.Figure(data=[go.Pie(
 fig_pie.update_layout(
         title=title,
         margin=dict(t=50, b=0, l=0, r=0),
-        showlegend=False
+        showlegend=True
     )
 
     # Affichage Streamlit
