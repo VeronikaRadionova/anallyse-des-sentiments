f1900b71e62de2b35ab8b8a2fbf9361e0b8d86c3
VeronikaRadionova <114427643+VeronikaRadionova@users.noreply.github.com>
2025-05-22 21:52:51 +0200

    try

--- app.py
+++ app.py
@@ -72,8 +72,48 @@ def get_roberta_label(text):
             return 'unknown'
     except:
         return 'neutral'  # fallback en cas d'erreur'''
+    
 
+# Importation du data dans le repertoire CSV
+dataframes = {"tweets_with_sentiments"}
+labels = {
+    "TRECIS-CTIT-H-001": "fireColorado2012",
+    "TRECIS-CTIT-H-002": "costaRicaEarthquake2012",
+    "TRECIS-CTIT-H-003": "floodColorado2013",
+    "TRECIS-CTIT-H-004": "typhoonPablo2012",
+    "TRECIS-CTIT-H-005": "laAirportShooting2013",
+    "TRECIS-CTIT-H-006": "westTexasExplosion2013",
+    "TRECIS-CTIT-H-007": "guatemalaEarthquake2012",
+    "TRECIS-CTIT-H-008": "italyEarthquakes2012",
+    "TRECIS-CTIT-H-009": "philipinnesFloods2012",
+    "TRECIS-CTIT-H-010": "albertaFloods2013",
+    "TRECIS-CTIT-H-011": "australiaBushfire2013",
+    "TRECIS-CTIT-H-012": "bostonBombings2013",
+    "TRECIS-CTIT-H-013": "manilaFloods2013",
+    "TRECIS-CTIT-H-014": "queenslandFloods2013",
+    "TRECIS-CTIT-H-015": "typhoonYolanda2013",
+    "TRECIS-CTIT-H-016": "joplinTornado2011",
+    "TRECIS-CTIT-H-017": "chileEarthquake2014",
+    "TRECIS-CTIT-H-018": "typhoonHagupit2014",
+    "TRECIS-CTIT-H-019": "nepalEarthquake2015",
+    "TRECIS-CTIT-H-020": "flSchoolShooting2018",
+    "TRECIS-CTIT-H-021": "parisAttacks2015",
+    "TRECIS-CTIT-H-022": "floodChoco2019",
+    "TRECIS-CTIT-H-023": "fireAndover2019",
+    "TRECIS-CTIT-H-024": "earthquakeCalifornia2014",
+    "TRECIS-CTIT-H-025": "earthquakeBohol2013",
+    "TRECIS-CTIT-H-026": "hurricaneFlorence2018",
+    "TRECIS-CTIT-H-027": "shootingDallas2017",
+    "TRECIS-CTIT-H-028": "fireYMM2016",
+    "TRECIS-CTIT-H-029": "albertaWildfires2019",
+    "TRECIS-CTIT-H-030": "cycloneKenneth2019",
+    "TRECIS-CTIT-H-031": "philippinesEarthquake2019",
+    "TRECIS-CTIT-H-032": "coloradoStemShooting2019",
+    "TRECIS-CTIT-H-033": "southAfricaFloods2019",
+    "TRECIS-CTIT-H-034": "sandiegoSynagogueShooting2019"
+}
 
+label_to_code = {v: k for k, v in labels.items()}
 
 # MAIN AFFICHAGE
 def analyse_sentiments(dataframes, labels): 
@@ -186,6 +226,184 @@ def analyse_sentiments(dataframes, labels):
     )
 
     st.plotly_chart(fig, use_container_width=True)
+
+analyse_sentiments(dataframes, labels)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from transformers import pipeline
+
+# TextBlob
+def analyze_textblob(text):
+    polarity = TextBlob(text).sentiment.polarity
+    if polarity > 0.05:
+        label = 'positive'
+    elif polarity < -0.05:
+        label = 'negative'
+    else:
+        label = 'neutral'
+    return polarity, label
+
+
+# VADER
+vader = SentimentIntensityAnalyzer()
+
+def analyze_vader(text):
+    scores = vader.polarity_scores(text)
+    compound = scores['compound']
+    if compound > 0.05:
+        label = 'positive'
+    elif compound < -0.05:
+        label = 'negative'
+    else:
+        label = 'neutral'
+    return compound, label
+
+
+roberta_sentiment = pipeline(
+    "sentiment-analysis",
+    model="cardiffnlp/twitter-roberta-base-sentiment"
+)
+
+# RoBERTa
+def get_roberta_label(text):
+    try:
+        result = roberta_sentiment(text)
+        label = result[0]['label'].lower()
+        if label == 'label_0':
+            return 'negative'
+        elif label == 'label_1':
+            return 'neutral'
+        elif label == 'label_2':
+            return 'positive'
+        else:
+            return 'unknown'
+    except:
+        return 'neutral'  # fallback en cas d'erreur
+
+
+# chargement du fichier CSV
+df = pd.read_csv('Tweet_clean.csv')
+df['clean_text'] = df['text'].apply(clean_text_for_sentiment)
+
+
+# analyse TextBlob
+df[['textblob_polarity', 'textblob_label']] = df['clean_text'].apply(
+    lambda x: pd.Series(analyze_textblob(x))
+)
+
+# analyse VADER
+df[['vader_compound', 'vader_label']] = df['clean_text'].apply(
+    lambda x: pd.Series(analyze_vader(x))
+)
+
+# analyse RoBERTa
+#df['roberta_label'] = df['clean_text'].apply(get_roberta_label)
+'''
+
+
+
+
+
+
+
+
+
+
+'''# calcul du score moyen par topic
+    topic_sentiment = df.groupby('topic')['vader_score'].mean().sort_values().reset_index()
+
+    # affichage
+    fig = px.bar(
+        topic_sentiment,
+        x='vader_score',
+        y='topic',
+        orientation='h',
+        title='Average Topic Sentiment (RoBERTa)',
+        labels={'vader_score': 'Average Sentiment', 'topic': 'Topic'},
+        color='vader_score',
+        color_continuous_scale='RdYlGn',
+    )
+
+    fig.update_layout(
+        xaxis_title='Average Sentiment',
+        yaxis_title='Topic',
+        template='plotly_white'
+    )
+
+    st.plotly_chart(fig, use_container_width=True)'''
+
+
+
+
+
+
+
+
+
+'''# TextBlob
+textblob_counts = df['textblob_label'].value_counts().reset_index()
+textblob_counts.columns = ['Sentiment', 'Tweets']
+textblob_counts['Color'] = textblob_counts['Sentiment'].map(color_map)
+
+fig_textblob = px.bar(textblob_counts, 
+                      x='Sentiment', y='Tweets',
+                      title="TextBlob Sentiment Distribution",
+                      color='Sentiment', color_discrete_map = color_map)
+fig_textblob.update_layout(barmode='stack')
+
+# VADER
+vader_counts = df['vader_label'].value_counts().reset_index()
+vader_counts.columns = ['Sentiment', 'Tweets']
+vader_counts['Color'] = vader_counts['Sentiment'].map(color_map)
+
+fig_vader = px.bar(vader_counts, 
+                   x='Sentiment', y='Tweets',
+                   title="VADER Sentiment Distribution",
+                   color='Sentiment', color_discrete_map = color_map)
+fig_vader.update_layout(barmode='stack')
+
+st.plotly_chart(fig_textblob)
+st.plotly_chart(fig_vader)'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
